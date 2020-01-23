2d3b27f8ad0a5c0d8dc8626c29325544212fd55a
ing <inyoman_gurnitha@yahoo.com>
2020-01-23 11:59:45 +0700

    8. Created Sitesetting model, superadmin, added sosmed addresses in db

--- appmain/models.py
+++ appmain/models.py
@@ -1,3 +1,18 @@
+from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class SiteSetting(models.Model):
+
+	sitename = models.CharField(max_length=150)
+	fb 		= models.CharField(max_length=150)
+	tw 		= models.CharField(max_length=150)
+	ig 		= models.CharField(max_length=150)
+	yt 		= models.CharField(max_length=150)
+	phone 	= models.CharField(max_length=150)
+	website 	= models.CharField(max_length=150)
+	set_name = models.CharField(max_length=150)
+
+	def __str__(self):
+		return self.set_name + ' | ' + str(self.pk)
